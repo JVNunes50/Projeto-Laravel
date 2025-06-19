b26ba649a70749ae45edc8c1c1ff0cdeb98c7cbe
JVNunes50 <jvnunes1944@gmail.com>
2025-06-18 21:05:00 -0300

    Foi criado a tela de login

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <header>
+        <h3 class="cabecalho">Projeto Laravel</h3>
+    </header>
+    <div class="texto">
+        <p>É um prazer receber sua visita. Em nosso site, você encontrará todas as informações sobre nossas soluções, serviços e o compromisso que temos com a excelência. Navegue por nossas seções para descobrir como podemos ajudar você a alcançar seus objetivos. Estamos à sua disposição.</p>
+        <p>ASS: Gemini</p>
+    </div>
+    <div class="login">
+        <form action="" method="post">
+            <label for="login">Login:</label>
+            <input type="text" name="login" id="id_login">
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha_id">
+            <input type="button" value="Acessar">
+        </form>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function(){
+    return view('layouts.login');
+});
+
+Route::get('app', function(){
+    return view('layouts.app');
+});
